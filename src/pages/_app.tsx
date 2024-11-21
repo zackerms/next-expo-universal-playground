a2e0bedadd61941bde7356143066dcb23d4ca1e3
zacker <iszkmasaki@gmail.com>
2024-11-21 12:59:14 +0900

    feat: 不要なコンポーネントを削除

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,5 @@
 import type { AppProps } from "next/app";
-import { TamaguiProvider, View } from '@tamagui/core'
+import { TamaguiProvider } from '@tamagui/core'
 import config from '../../tamagui.config'
 
 export default function App({ Component, pageProps }: AppProps) {
